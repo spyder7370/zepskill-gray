a14ab902379bf830e8038a3b0ebda5526f3b3192
Hitesh Yadav <53417879+spyder7370@users.noreply.github.com>
2023-07-12 08:23:48 +0530

    react

---
+++ react/basic-1/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// functional component
+const App = () => {
+	return (
+		<React.Fragment>
+			<h1>hello</h1>
+			<p>world</p>
+		</React.Fragment>
+	);
+};
+
+// class based component
+export default App;
